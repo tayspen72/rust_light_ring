c0c7c812cb9d0447456ed1c8edb07e106912e6ed
Taylor Spencer <tayspen72@gmail.com>
2020-01-02 11:30:56 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+// dev profile: easier to debug panics; can put a breakpoint on `rust_begin_unwind`
+#[cfg(debug_assertions)]
+extern crate panic_halt;
+
+// release profile: minimize the binary size of the application
+#[cfg(not(debug_assertions))]
+extern crate panic_abort;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::(debug, hprintln);
+
+#[entry]
+fn main() -> ! {
+    hprintln!("Launching light ring").unwrap();
+
+    loop {
+        // your code goes here
+    }
+}
