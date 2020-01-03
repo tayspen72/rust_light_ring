8484c386f27693124fa9667023b7517de8d023db
Taylor Spencer <tayspen72@gmail.com>
2020-01-02 22:45:19 -0700

    Working on IOPORT defines, methods, and syntax

--- src/ioport.rs
+++ src/ioport.rs
@@ -1,21 +1,66 @@
-//STM32F3Discovery LED Ring, defined from top going clockwise around the ring
-const _LED0: u16 = 0x0200u16;	//PE9, LD3, North: Red
-const _LED1: u16 = 0x0400u16;	//PE10, LD5, North-East: Orange
-const _LED2: u16 = 0x0800u16;	//PE11, LD7, East: Green
-const _LED3: u16 = 0x1000u16;	//PE12, LD9, South-East: Blue
-const _LED4: u16 = 0x2000u16;	//PE13, LD10, South: Red
-const _LED5: u16 = 0x4000u16;	//PE14. LD8, South-West: Orange
-const _LED6: u16 = 0x8000u16;	//PE15, LD6, West: Green
-const _LED7: u16 = 0x0100u16;	//PE8, LD4, North-West: Blue
+use volatile_register::{RW, RO};
 
-enum LED_State{
-	LED_On = 0b1,
-	LED_Off = 0b0
+//STM32F3Discovery LED Ring, defined from top going clockwise around the ring
+enum Led{
+	Led0 = 0x0200,		//PE 9, LD3, North: Red
+	Led1 = 0x0400,		//PE10, LD5, North-East: Orange
+	Led2 = 0x0800,		//PE11, LD7, East: Green
+	Led3 = 0x1000,		//PE12, LD9, South-East: Blue
+	Led4 = 0x2000,		//PE13, LD10, South: Red
+	Led5 = 0x4000,		//PE14. LD8, South-West: Orange
+	Led6 = 0x8000,		//PE15, LD6, West: Green
+	Led7 = 0x0100,		//PE 8, LD4, North-West: Blue
+}
+
+pub enum PinState{
+	PinSet = 0b1,
+	PinReset = 0b0
+}
+
+pub enum PinMode{
+	ModeInput,
+	ModeOutput,
+	ModeAlternateFunction,
+	ModeAnalog
+}
+
+pub enum PortName{
+	PortA,
+	PortB,
+	PortC,
+	PortD,
+	PortE,
+	PortF,
+	PortG,
+	PortH
 }
 
 //tell the compile to assemble this struct in c-like manner
 //  ie: contiguous in memory as expected
 #[repr(C)]
 struct IOPort{
-    pub 
+	pub moder: RW<u32>,
+	pub otyper: RW<u32>, 
+	pub ospeedr: RW<u32>,
+	pub pupdr: RW<u32>,
+	pub idr: RO<u32>,
+	pub odr: RW<u32>, 
+	pub bsrr: WO<u32>,
+	pub lckr: RW<u32>,
+	pub afrl: RW<u32>,
+	pub afrh: RW<u32>,
+	pub brr: WO<u32>
+}
+
+/** Private Functions **/
+
+
+/** Public Functions **/
+pub fn InitPins(port: PortName, pins: u16, mode: PinMode){
+	let val = match mode{
+		PinMode::ModeInput => 0x0
+		PinMode::ModeOutput => 0x1
+		PinMode::ModeAlternateFunction => initAltFuncPins(port, pins),
+		PinMode::ModeAnalog => initAnalogPins(port, pins),
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+//declare external source files
+mod ioport;
+
 // pick a panicking behavior
 // dev profile: easier to debug panics; can put a breakpoint on `rust_begin_unwind`
 #[cfg(debug_assertions)]
@@ -11,12 +14,14 @@ extern crate panic_halt;
 extern crate panic_abort;
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::(debug, hprintln);
+use cortex_m_semihosting::{debug, hprintln};
 
 #[entry]
 fn main() -> ! {
     hprintln!("Launching light ring").unwrap();
 
+
+    
     loop {
         // your code goes here
     }
