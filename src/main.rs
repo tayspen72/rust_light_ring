aeeb297e336484f27b8bea2dd8d69200ff2714f2
Taylor Spencer <tspencer@prismview.com>
2020-01-06 15:20:56 -0700

    Updated to a more clean led update function, includes SYSTICK for delay, not tested

--- src/main.rs
+++ src/main.rs
@@ -10,117 +10,94 @@ extern crate panic_semihosting;
 #[cfg(not(debug_assertions))]
 extern crate panic_abort;
 
-use cortex_m_rt::entry;
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m_rt::{entry, exception};
 #[allow(unused_imports)]
 use cortex_m_semihosting::{debug, hprintln};
+
+fn init_gpio_e(p: &stm32f30x::Peripherals){
+    unsafe {p.GPIOE.moder.write(|w| w.bits(0x5555_0000));}
+    unsafe {p.GPIOE.bsrr.write(|w| w.bits(0xFF00_0000));}
+}
+
+fn init_rcc(p: &stm32f30x::Peripherals){ 
+    //enable GPIO Port E
+    p.RCC.ahbenr.write(|w| w.iopeen().set_bit());
+}
+
+fn init_systick(){
+    let mut p = cortex_m::Peripherals::take().unwrap();
+    p.SYST.set_clock_source(SystClkSource::Core);
+    p.SYST.set_reload(2_097_151); // base 8MHz, (2^22) - 1 = ~0.26 seconds
+    p.SYST.enable_counter();
+    p.SYST.enable_interrupt();
+}
+
+static mut UPDATE_LEDS: bool = false;
+
+#[exception]
+fn SysTick(){
+    unsafe {UPDATE_LEDS = true;}
+}
 
 #[entry]
 fn main() -> ! {
     let peripheral = stm32f30x::Peripherals::take().unwrap();
 
-    peripheral.RCC.ahbenr.write(|w| w.iopeen().set_bit());
-
-    unsafe {peripheral.GPIOE.moder.write(|w| w.bits(0x5555_0000));}
-    unsafe {peripheral.GPIOE.bsrr.write(|w| w.bits(0xFF00_0000));}
-
-    const DELAY_MAX: u32 = 25_000;
-    let mut delay: u32;
-    let mut state: u8 = 0;
-    let mut output: u8 = 1;
+    init_rcc(&peripheral);
+    init_gpio_e(&peripheral);
+    init_systick();
 
     loop {
-        //arbitrary delay
-        delay = DELAY_MAX;
-        while delay > 0 {
-            delay = delay - 1;
-        }
-
-        //update output display
-        match state {
-            0 => {  //PE 9, LD3, North: Red
-                if output == 0 {
-                    peripheral.GPIOE.bsrr.write(|w| w.br9().reset());
-                }
-                else {
-                    peripheral.GPIOE.bsrr.write(|w| w.bs9().set());
-                }
-            },
-            1 => {  //PE10, LD5, North-East: Orange
-                if output == 0 {
-                    peripheral.GPIOE.bsrr.write(|w| w.br10().reset());
-                }
-                else {
-                    peripheral.GPIOE.bsrr.write(|w| w.bs10().set());
-                }
-            },
-            2 => {  //PE11, LD7, East: Green
-                if output == 0 {
-                    peripheral.GPIOE.bsrr.write(|w| w.br11().reset());
-                }
-                else {
-                    peripheral.GPIOE.bsrr.write(|w| w.bs11().set());
-                }
-            },
-            3 => {  //PE12, LD9, South-East: Blue
-                if output == 0 {
-                    peripheral.GPIOE.bsrr.write(|w| w.br12().reset());
-                }
-                else {
-                    peripheral.GPIOE.bsrr.write(|w| w.bs12().set());
-                }
-            },
-            4 => {  //PE13, LD10, South: Red
-                if output == 0 {
-                    peripheral.GPIOE.bsrr.write(|w| w.br13().reset());
-                }
-                else {
-                    peripheral.GPIOE.bsrr.write(|w| w.bs13().set());
-                }
-            },
-            5 => {  //PE14. LD8, South-West: Orange
-                if output == 0 {
-                    peripheral.GPIOE.bsrr.write(|w| w.br14().reset());
-                }
-                else {
-                    peripheral.GPIOE.bsrr.write(|w| w.bs14().set());
-                }
-            },
-            6 => {  //PE15, LD6, West: Green
-                if output == 0 {
-                    peripheral.GPIOE.bsrr.write(|w| w.br15().reset());
-                }
-                else {
-                    peripheral.GPIOE.bsrr.write(|w| w.bs15().set());
-                }
-            },
-            _ => {  //PE 8, LD4, North-West: Blue
-                if output == 0 {
-                    peripheral.GPIOE.bsrr.write(|w| w.br8().reset());
-                }
-                else {
-                    peripheral.GPIOE.bsrr.write(|w| w.bs8().set());
-                }
-            }
-        }
-
-        //set up conditions
-        state = state + 1;
-        if state == 8 {
-            state = 0;
-
-            if output == 0 {
-                output = 1;
-            }
-            else {
-                output = 0;
+        unsafe{
+            if UPDATE_LEDS == true {
+                UPDATE_LEDS = false;
+                update_led(&peripheral);
             }
         }
     }
 }
 
-//STM32F3Discovery LED Ring, defined from top going clockwise around the ring
-// pub const LED0: u16 = 0x0200;		//PE 9, LD3, North: Red
-// pub const LED1: u16 = 0x0400;		//PE10, LD5, North-East: Orange
-// pub const LED2: u16 = 0x0800;		//PE11, LD7, East: Green
-// pub const LED3: u16 = 0x1000;		//PE12, LD9, South-East: Blue
-// pub const LED4: u16 = 0x2000;		//PE13, LD10, South: Red
+fn update_led(p: &stm32f30x::Peripherals){
+    static mut PIN: u8 = 0;
+    static mut STATE: u8 = 1;
+
+    unsafe{
+        write_led(&p, PIN, STATE);
+
+        //update conditions
+        PIN = PIN + 1;
+        if PIN == 8 {
+            PIN = 0;
+
+            if STATE == 0 {
+                STATE = 1;
+            }
+            else {
+                STATE = 0;
+            }
+        }
+    }
+}
+
+
+fn write_led(p: &stm32f30x::Peripherals, led: u8, output_state: u8){
+    //convert led to bit mask based on pin layout
+    let mut mask: u32 = match led{
+        0 => (1 << 9),
+        1 => (1 << 10),
+        2 => (1 << 11),
+        3 => (1 << 12),
+        4 => (1 << 13),
+        5 => (1 << 14),
+        6 => (1 << 15),
+        _ => (1 << 8)
+    };
+
+    //if turning off, shift up  to reset bits
+    if output_state == 0 {
+        mask = mask << 16;
+    }
+
+    unsafe { p.GPIOE.bsrr.write(|w| w.bits(mask)); }
+}
